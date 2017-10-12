4fcf8d790d9c73f09cf379c3b7570d508a561f0c
AlekseyPriz <aleksey.priz@gmail.com>
2017-10-12 11:52:59 +0300

    Protractor - PageObject
    Улучшение кода e2e тестов включает:
    
    -Применен патерн PageObject. Сформировано две класса для /view1 и
    /myaccount.
    
    - Изменен идентификатор для поиска покемонов.

--- e2e-tests/test.js
+++ e2e-tests/test.js
@@ -1,59 +1,111 @@
 const expext = require('chai').expect;
 
+let ViewPage = function () {
+
+  let list = element.all(by.repeater('item in menu'));
+  let linkElement = element(by.id('pokemon-list')).$('.ng-binding.active');
+  let pokemonList = element.all(by.repeater('pokemon in pokemons'));
+
+  this.get = function () {
+     return browser.get('http://localhost:8000/#/view1')
+  };
+
+  this.getFirstLinkText = function () {
+    return list.first().getText();
+  };
+
+  this.getActiveLinkText = function () {
+    return linkElement.getText();
+  };
+
+  this.pokemonListCount = function () {
+    return pokemonList.count();
+  }
+};
+
+let MyAccountPage = function () {
+
+  let userForm = element(by.id('userForm'));
+  let inputName = element(by.id('inputName'));
+  let inputEmail = element(by.id('inputEmail'));
+  let inputPhonenomber = element(by.model('user.phone'));
+
+  this.get = function () {
+    return browser.get('http://localhost:8000/#/myaccount')
+  };
+
+  this.userFormIsPresent = function () {
+    return userForm.isPresent();
+  };
+
+  this.AttrIsRequired = function (input) {
+    if (input === 'name') {
+      return inputName.getAttribute('required');
+    } else if (input === 'email') {
+      return inputEmail.getAttribute('required');
+    } else if (input === 'phoneNomber'){
+      return inputPhonenomber.getAttribute('required');
+    }
+  };
+
+};
+
 describe('e2e тесты приложение myApp', function() {
   it('Первая ссылка имеет текст "-view1-" ', function() {
 
-    browser.get('http://localhost:8000');
-    let list = element.all(by.repeater('item in menu'));
-    expect(list.first().getText()).toEqual('-view1-');
+    let viewPage = new ViewPage();
+    viewPage.get();
+
+    expect(viewPage.getFirstLinkText()).toEqual('-view1-');
 
   });
 
   it('Активная ссылка "-view1-" имеет класс ".ng-binding.active" ', function() {
 
-    browser.get('http://localhost:8000/#/view1');
-    let child = element(by.css('.ng-scope')).$('.ng-binding.active');
-    expect(child.getText()).toBe('-view1-');
+    let viewPage = new ViewPage();
+    viewPage.get();
+
+
+    expect(viewPage.getActiveLinkText()).toBe('-view1-');
 
   });
 
   it('По адрессу /myaccount открывается форма" ', function() {
 
-    browser.get('http://localhost:8000/#/myaccount');
-    expect(element(by.id('userForm')).isPresent()).toBe(true);
+    let myAccountPage = new MyAccountPage();
+    myAccountPage.get();
+
+    expect(myAccountPage.userFormIsPresent()).toBe(true);
 
   });
 
   it('Все Input по адрессу /myaccount имеют атрибут "required" ', function() {
 
-    browser.get('http://localhost:8000/#/myaccount');
+    let myAccountPage = new MyAccountPage();
+    myAccountPage.get();
 
-    let inputName = element(by.id('inputName'));
-    expect(inputName.getAttribute('required')).toBeTruthy();
-
-    let inputEmail = element(by.id('inputEmail'));
-    expect(inputEmail.getAttribute('required')).toBeTruthy();
-
-    let inputPhonenomber = element(by.model('user.phone'));
-    expect(inputPhonenomber.getAttribute('required')).toBeTruthy();
+    expect(myAccountPage.AttrIsRequired('name')).toBeTruthy();
+    expect(myAccountPage.AttrIsRequired('email')).toBeTruthy();
+    expect(myAccountPage.AttrIsRequired('phoneNomber')).toBeTruthy();
 
   });
 
   it('Показано 5 покемонов', function() {
 
-    browser.get('http://localhost:8000/#/view1');
-    let pokemonList = element.all(by.repeater('pokemon in pokemons'));
-    expect(pokemonList.count()).toBe(5);
+    let viewPage = new ViewPage();
+    viewPage.get();
 
+    expect(viewPage.pokemonListCount()).toBe(5);
 
   });
 
   it('Показан 1 покемон', function() {
 
-    browser.get('http://localhost:8000/#/view1');
+    let viewPage = new ViewPage();
+    viewPage.get();
+
     element(by.model('myQuery')).sendKeys('Pik');
-    let pokemonList = element.all(by.repeater('pokemon in pokemons'));
-    expect(pokemonList.count()).toBe(1);
+    expect(viewPage.pokemonListCount()).toBe(1);
 
   });
 
